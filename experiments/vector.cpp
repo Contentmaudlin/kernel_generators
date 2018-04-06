09bd035344b58c6cb3dfe02680640db54bdb4d2d
John Hui <jzh2106@gmail.com>
2018-04-05 22:00:27 -0400

    commnted vector code

--- experiments/vector.cpp
+++ experiments/vector.cpp
@@ -19,16 +19,16 @@ int main(void)
     auto p = v.end() - 1;   // access iterator pointing to new last element
     auto q = v.end();       // access iterator pointing to end
 
-    cout << "[BEFORE]: i{v.end() - 1} --> " << *i << endl;
-    cout << "[BEFORE]: j{v.end()} --> " << *j << endl;
+    cout << "[BEFORE]: i{v.end() - 1} --> " << *i << endl;  // prints 3
+    cout << "[BEFORE]: j{v.end()} --> " << *j << endl;      // prints hel
 
     cout << "v.push_back(\"hel\");" << endl;
 
-    cout << "[AFTER]: p{v.end() - 1} --> " << *p << endl;
-    cout << "[AFTER]: q{v.end()} --> " << *q << endl;
+    cout << "[AFTER]: p{v.end() - 1} --> " << *p << endl;   // prints hel
+    cout << "[AFTER]: q{v.end()} --> " << *q << endl;       // prints nothing
 
-    cout << "j == q: " << (j == q) << endl;
-    cout << "j == p: " << (j == p) << endl;
+    cout << "j == q: " << (j == q) << endl;                 // prints 0
+    cout << "j == p: " << (j == p) << endl;                 // prints 1
 
     return 0;
 }
