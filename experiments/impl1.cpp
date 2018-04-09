8f5fdaa1f57e43708589dc680775d176746de2f5
John Hui <jzh2106@gmail.com>
2018-04-09 01:36:23 -0400

    fibonacci

--- experiments/impl1.cpp
+++ experiments/impl1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +13,38 @@ protected:
 	// do nothing for now
 	Gn(void) : ctr{0} {}
 
+	template <typename Type, int LookbackMax>
+	struct Lookback {
+		std::vector<Type> circular_buf;
+
+		Lookback(const Type& init) : circular_buf(LookbackMax, init) {}
+
+		// ideally the user can't access this
+		const Type& get(int lbi, int ctr) const
+		{
+			// static_assert that lbi < LookbackMax
+			return circular_buf[(LookbackMax + ctr - lbi) % LookbackMax];
+		}
+
+		void set(const Type& val, int ctr)
+		{
+			circular_buf[ctr % LookbackMax] = val;
+		}
+	};
+
+	template <typename Type, int LookbackMax>
+	const Type& lb(const struct Lookback<Type, LookbackMax>& lbv, int lbi)
+	{
+		return lbv.get(lbi, ctr);
+	}
+
+	template <typename Type, int LookbackMax>
+	const Type& lset(struct Lookback<Type, LookbackMax>& lbv, const Type& val)
+	{
+		lbv.set(val, ctr);
+		return val;
+	}
+
 public:
 	const Ret yield(void)
 	{
@@ -21,21 +55,28 @@ public:
 };
 
 
-class MyGn : public Gn<int> {
-	int i;
+class Fib : public Gn<int> {
+	Lookback<int, 4> i;
 	int iterate(void)
 	{
-		return i;
+		lset(i, lb(i, 1) + lb(i, 2));
+		return lb(i, 0);
 	}
 public:
-	MyGn(int _i) : Gn{}, i{_i} {}
+	Fib(int _i) : Gn{}, i{_i} {}
 };
 
 int main(void)
 {
 
-	MyGn g{1}; // only ever yield 1
+	Fib g{1}; // only ever yield 1
 
+	cout << g.yield() << endl;
+	cout << g.yield() << endl;
+	cout << g.yield() << endl;
+	cout << g.yield() << endl;
+	cout << g.yield() << endl;
+	cout << g.yield() << endl;
 	cout << g.yield() << endl;
 	cout << g.yield() << endl;
 
