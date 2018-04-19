25527144e244b235f721b99c6e9d2ba8a7ca913f
Mert Ussakli <ussaklimert@gmail.com>
2018-04-19 12:29:54 -0400

    :%s/\t/  /g

--- test/tests/prime.cc
+++ test/tests/prime.cc
@@ -3,11 +3,11 @@
 #include "prime.hpp"
 
 TEST(Prime, BasicOperation) {
-	prime_gen g;
-	EXPECT_EQ(2, *g);
-	EXPECT_EQ(3, *++g);
-	EXPECT_EQ(5, *++g);
-	EXPECT_EQ(7, *++g);
-	EXPECT_EQ(11, *++g);
-	EXPECT_EQ(13, *++g);
+  prime_gen g;
+  EXPECT_EQ(2, *g);
+  EXPECT_EQ(3, *++g);
+  EXPECT_EQ(5, *++g);
+  EXPECT_EQ(7, *++g);
+  EXPECT_EQ(11, *++g);
+  EXPECT_EQ(13, *++g);
 }
