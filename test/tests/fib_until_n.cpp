35e3ad40d89eb256309e86a480d4287b7aad6ef4
John Hui <jzh2106@gmail.com>
2018-04-26 23:50:07 -0400

    added fib_until_n

---
+++ test/tests/fib_until_n.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <fib.hpp>
+
+using namespace std;
+using namespace kgen;
+
+
+const int iters = 100;
+
+int main() {
+
+    fib_gen g{};
+
+    unsigned long long cur = 1;
+    unsigned long long prv;
+    int ctr = 0;
+
+    for (auto x : g.until_n(iters)) {
+        assert(x == cur);
+        cur += prv;
+        prv = cur - prv;
+        ++ctr;
+    }
+
+    assert(ctr == iters);
+
+    return 0;
+}
