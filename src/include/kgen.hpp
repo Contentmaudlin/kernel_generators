92768d749cc3cf93902cc6867a33d6b5a60af06a
John Hui <jzh2106@gmail.com>
2018-04-27 00:52:08 -0400

    fixed return type of prev

--- src/include/kgen.hpp
+++ src/include/kgen.hpp
@@ -438,7 +438,7 @@ namespace kgen {
             }
 
             template<int I>
-            U prev() {
+            const U &prev() {
                 static_assert(I < 0, "can only lookback");
                 static_assert(-I <= Max);
                 return buf[(_lb_base::ctr + I + Max + 1) % (Max + 1)];
@@ -497,7 +497,7 @@ namespace kgen {
             }
 
             template<int I>
-            U prev() { return lb_base::template get<U, Max>()->template prev<I>(); }
+            const U &prev() { return lb_base::template get<U, Max>()->template prev<I>(); }
 
             const U &operator[](int i) { return lb_base::template get<U, Max>()->operator[](i); }
 
@@ -558,10 +558,10 @@ namespace kgen {
 
         virtual T next() { throw reached_eog("Generator at end"); }
 
-        T prev(int i) { return state->val[i]; }
+        const T &prev(int i) { return state->val[i]; }
 
         template<int I>
-        T prev() { return state->val.template prev<I>(); }
+        const T &prev() { return state->val.template prev<I>(); }
 
     public:
         const T &operator*() override {
